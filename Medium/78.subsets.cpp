2b4dc44a7e409c0b393eeefb0ce3ba9ef4f2ff30
GEARdHAX <adarsharya2911@gmail.com>
2026-01-21 23:03:27 +0530

    Solved : Leetcode 78

---
+++ Medium/78.subsets.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=78 lang=cpp
+ *
+ * [78] Subsets
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void set(vector<int> &nums, vector<int> output, int index,
+             vector<vector<int>> &ans)
+    {
+
+        // base case
+        if (index == nums.size())
+        {
+            ans.push_back(output);
+            return;
+        }
+
+        // excluding current element
+        set(nums, output, index + 1, ans);
+
+        // including current element
+        output.push_back(nums[index]);
+        set(nums, output, index + 1, ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        set(nums, output, 0, ans);
+        return ans;
+    }
+};
+// @lc code=end
+
