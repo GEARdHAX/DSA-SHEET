e50e23aa7ae14e27f6ef12a61ea325a47f273712
GEARdHAX <adarsharya2911@gmail.com>
2026-02-06 23:41:30 +0530

    Solved : Leetcode 83

--- Concepts/Doubly_Linked_List.cpp
+++ Concepts/Doubly_Linked_List.cpp
@@ -67,7 +67,63 @@ void insertAtTail(Node *&head, Node *&tail, int d)
     tail->next = newNode;
     tail = newNode;
 }
+void insertAtPos(Node *&head, Node *&tail, int pos, int d)
+{
+    if (pos == 1)
+    {
+        insertAtHead(head, tail, d);
+        return;
+    }
 
+    Node *temp = head;
+    int cnt = 1;
+
+    while (cnt < pos - 1 && temp != NULL)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    // Invalid position
+    if (temp == NULL)
+        return;
+
+    if (temp->next == NULL)
+    {
+        insertAtTail(head, tail, d);
+        return;
+    }
+
+    Node *newNode = new Node(d);
+    newNode->next = temp->next;
+    temp->next->prev = newNode;
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+void deleteNode(int pos, Node *&head)
+{
+    if (pos == 1)
+    {
+        Node *temp = head;
+        temp->next->prev = NULL;
+        head = temp->next;
+        temp->next = NULL;
+        delete temp;
+    }
+    int cnt = 1;
+    Node *curr = head;
+    Node *prev = NULL;
+    while (cnt < pos)
+    {
+        prev = curr;
+        curr = curr->next;
+        cnt++;
+    }
+    curr->prev = NULL;
+    prev->next = curr->next;
+    curr->next = NULL;
+    delete curr;
+}
 int main()
 {
     Node *node1 = new Node(10);
@@ -76,9 +132,19 @@ int main()
     insertAtHead(head, tail, 20);
     insertAtHead(head, tail, 30);
     insertAtHead(head, tail, 40);
+    cout << "HEAD : ";
     printNode(head);
     insertAtTail(head, tail, 50);
     insertAtTail(head, tail, 60);
     insertAtTail(head, tail, 70);
+    cout << "TAIL : ";
+    printNode(head);
+    insertAtPos(head, tail, 5, 80);
+    insertAtPos(head, tail, 1, 90);
+    insertAtPos(head, tail, 11, 100);
+    cout << "POSITION : ";
+    printNode(head);
+    cout << "DELETE : ";
+    deleteNode(9, head);
     printNode(head);
 }
